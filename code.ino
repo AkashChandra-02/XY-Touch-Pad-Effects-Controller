9f2e19bffe7a44eea85652e4a9859c472b2b12d1
Akash Chandra <27438862+AkashChandra-02@users.noreply.github.com>
2022-03-10 18:16:30 -0600

    updated setup to use a loop

--- code.ino
+++ code.ino
@@ -4,15 +4,16 @@
 #define NUM_C 4 
 //analogWrite(motor, 0);
 
-int pots[4] = {0, 0, 0, 0};
+int pot_value[4] = {0, 0, 0, 0};
 int pot_pins = {A0, A1, A2, A3};
 int out_pins = {3, 5, 6, 9};
 
 void setup() {
-	pinMode(C1, OUTPUT);
-	pinMode(C2, OUTPUT);
-	pinMode(C3, OUTPUT);
-	pinMode(C4, OUTPUT);
+	
+	for(int i = 0; i < NUM_C; i++)
+	{
+		pinMode(out_pins[i], OUTPUT);
+	}
 }
 
 void loop() {
@@ -20,18 +21,18 @@ void loop() {
 	//Read in analog values
 	for(int i = 0; i < NUM_POTS; i++)
 	{
-		pots[i] = analogRead(pots_pin[i]);
+		pot_value[i] = analogRead(pots_pin[i]);
 	}
 	
 	//Modify the values if needed
 	for(int i = 0; i < NUM_POTS; i++)
 	{
-		pots[i] = map(pots[i], 0, 1023, 0, 255);
+		pot_value[i] = map(pot_value[i], 0, 1023, 0, 255);
 	}
 	
 	//Write values out to PWM pins
 	for(int i = 0; i < NUM_C; i++)
 	{
-		analogWrite(out_pins[i], pots[i]);
+		analogWrite(out_pins[i], pot_value[i]);
 	}
 }
